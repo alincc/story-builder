2cf3caa096b05a294ff9e43c9e03167d354dfb13
Wagner Camarao <wagner@camarao.io>
2015-05-16 15:18:02 -0700

    Stub passages

--- app/modules/passage/passage-index.controller.js
+++ app/modules/passage/passage-index.controller.js
@@ -3,12 +3,21 @@
 angular.module('app.passage')
   .controller('PassageIndexController', PassageIndexController);
 
-function PassageIndexController(PassageService) {
+function PassageIndexController(Passage, PassageService) {
   var ctrl = this;
 
   ctrl.passages = PassageService.passages;
 
   ctrl.hasPassages = function() {
     return ctrl.passages.length > 0;
+  };
+
+  ctrl.stub = function() {
+    for (var p, i=0; i<10; i++) {
+      p = new Passage();
+      p.name = 'Passage ' + i;
+      p.description = 'Description ' + i;
+      ctrl.passages.push(p);
+    }
   };
 }

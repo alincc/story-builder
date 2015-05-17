7e2f8425dcd94453b07c291e3dce797eecdfec8a
Wagner Camarao <wagner@camarao.io>
2015-05-16 17:41:59 -0700

    Remove stub

--- app/modules/passage/passage-index.controller.js
+++ app/modules/passage/passage-index.controller.js
@@ -11,13 +11,4 @@ function PassageIndexController(Passage, PassageService) {
   ctrl.hasPassages = function() {
     return ctrl.passages.length > 0;
   };
-
-  ctrl.stub = function() {
-    for (var p, i=0; i<10; i++) {
-      PassageService.save({
-        name: 'Passage ' + i,
-        description: 'Description ' + i
-      });
-    }
-  };
 }
